67cfc202694035c6af67d127cb3d3a777af327bb
Paul Komaravalli <jpaulkom@its.jnj.com>
2020-06-28 15:00:20 +0530

    CHanges to the Contact Page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,47 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const ContactPage = ({ data, location }) => {
+const ContactPage = ({ data, location, props }) => {
   const siteTitle = data.site.siteMetadata.title
+  const [name, email, message] = useState("");
+
+  // This function puts all the form fields into a FormData constructor, which we later encode with the URLSearchParams constructor
+  const createFormDataObj = (data) => {
+    const formData = new FormData();
+    Object.keys(data).forEach((k)=>{
+      formData.append(k,data[k])
+    });
+    return formData
+  }
+
+  const handleSubmit = (e) => {
+    // This `data` object is what's passed to the createFormDataObj. It needs all of your form fields, where the key is the name= attribute and the value is the value=
+    const data = { 
+      "form-name": "contactN",
+      "name": name,
+      "email": email,
+      "message": message
+    }
+    // This POSTs your encoded form to Netlify with the required headers (for text; headers will be different for POSTing a file) and, on success, redirects to the custom success page using Gatsby's `navigate` helper function that we imported at the top
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(createFormDataObj(data)).toString()
+    })
+      .then(() => navigate("/thank-you"))
+      .catch(error => alert(error));
+    // This is required to prevent the default onSubmit behavior
+    e.preventDefault();
+  };
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="Contact" />
       <h1>Contact</h1>
-      <form name="contactUs" method="POST" data-netlify="true" enctype="application/x-www-form-urlencoded">
+      <form netlify action="/" name="contactN" method="post" onSubmit={handleSubmit}>
         <p>
           <label>Your Name: <input type="text" name="name" /></label>   
         </p>
---
+++ src/pages/thank-you.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="Thank You" />
+      <h1>Thank You</h1>
+      <p>Thank You</p>
+    </Layout>
+  )
+}
+
+export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
